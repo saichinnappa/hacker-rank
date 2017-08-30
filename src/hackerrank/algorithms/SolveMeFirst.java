bc11d2a3b6b676fd2fa20d51aa01e044f3ee2d11
Sai Chinnappa <saisubramanyam.purruchinnappa@target.com>
2017-08-29 21:09:04 -0500

    few problems

---
+++ src/hackerrank/algorithms/SolveMeFirst.java
@@ -0,0 +1,33 @@
+package hackerrank.algorithms;
+
+/**
+ * Input Format
+
+ Code that reads input from stdin is provided for you in the editor. There are  lines of input, and each line contains a single integer.
+
+ Output Format
+
+ Code that prints the sum calculated and returned by solveMeFirst is provided for you in the editor.
+
+
+ */
+
+import java.util.Scanner;
+
+public class SolveMeFirst {
+
+  static int solveMeFirst(int a, int b) {
+    return a+b;
+  }
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int a;
+    a = in.nextInt();
+    int b;
+    b = in.nextInt();
+    int sum;
+    sum = solveMeFirst(a, b);
+    System.out.println(sum);
+  }
+}
